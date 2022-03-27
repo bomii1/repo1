4727e49e7160782368c4030c26abeee3555c0b50
ihobbang250 <101381851+ihobbang250@users.noreply.github.com>
2022-03-28 02:32:36 +0900

    Update open source sw.py

--- open source sw.py
+++ open source sw.py
@@ -1,14 +1,13 @@
+Touched by ihobbang250
 #정보미 202103076 컴퓨터공학부
 a='오픈소스 sw 2주차 실습'
 print(a)
 b='오픈소스 sw 3주차 실습'
 print(b)
 #question
-c='practice'
-print(c)
+#c='practice'
+#print(c)
 
 import random
 yasick=['엽떡','닭발','치킨','피자','곱창','맥주']
-random.choice()
-
-print('4주차 pull request 실습입니다!')
+print('오늘의 야식 메뉴: {0}'.format(random.choice(yasick)))
